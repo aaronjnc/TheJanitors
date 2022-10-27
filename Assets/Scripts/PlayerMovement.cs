755ef932ecfafc74f9bed58001b4c3e3427d1a57
Imperite <dsbradley03@gmail.com>
2022-10-27 00:02:46 -0700

    separated input for ui to own file

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -12,9 +12,6 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField]
     private float speed;
     // Start is called before the first frame update
-
-    [SerializeField]
-    private GameObject pauseOverlay, hudOverlay;
     void Start()
     {
         rb = GetComponent<Rigidbody>();
@@ -23,8 +20,8 @@ public class PlayerMovement : MonoBehaviour
         controls.PlayerMovement.Movement.canceled += StopMove;
         controls.PlayerMovement.Movement.Enable();
 
-        controls.UI.Pause.performed += TogglePause;
-        controls.UI.Pause.Enable();
+        //controls.UI.Pause.performed += TogglePause;
+        //controls.UI.Pause.Enable();
     }
 
     void Move(CallbackContext ctx)
@@ -36,20 +33,5 @@ public class PlayerMovement : MonoBehaviour
     void StopMove(CallbackContext ctx)
     {
         rb.velocity = Vector3.zero;
-    }
-
-    void TogglePause(CallbackContext ctx)
-    {
-        if (pauseOverlay.activeInHierarchy)
-        {
-            Debug.Log("toggling");
-            pauseOverlay.SetActive(false);
-            hudOverlay.SetActive(true);
-        }else
-        {
-            pauseOverlay.SetActive(true);
-            hudOverlay.SetActive(false);
-        }
-
     }
 }
