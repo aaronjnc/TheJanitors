29f564919085a99e1feb1805c3808011070e9d83
buritzcold <benjaminrimon@gmail.com>
2022-11-01 16:13:02 -0400

    points update

--- Assets/Score.cs
+++ Assets/Score.cs
@@ -1,17 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
-public class ScoreManager : MonoBehaviour
+using TMPro;
+
+public class Score : MonoBehaviour
 {
+    public int score;
+    public TextMeshProUGUI scoreUI;
 
-    public Text scoreText;
-
-    int score = 0;
     // Start is called before the first frame update
     void Start()
     {
-        scoreText.text = score.ToString() + " points";
+        scoreUI.text = score.ToString() + " points";
     }
 
     // Update is called once per frame
