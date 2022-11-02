610c5fbdfed7d9820bf26d81ac8584b030b8ee26
Aaron <aaronkingnc@gmail.com>
2022-11-01 23:21:29 -0400

    Player and Mop animations

--- Assets/Scripts/AbilitySystem.cs
+++ Assets/Scripts/AbilitySystem.cs
@@ -9,9 +9,15 @@ public class AbilitySystem : MonoBehaviour
     PlayerMovement movement;
     [SerializeField]
     GameObject WetSpot;
+    Animator animator;
+    [SerializeField]
+    GameObject mop;
+    [SerializeField]
+    Vector3[] mopPositions;
     // Start is called before the first frame update
     void Start()
     {
+        animator = mop.GetComponent<Animator>();
         movement = GetComponent<PlayerMovement>();
         controls = new InputController();
         controls.PlayerMovement.Ability.performed += WetFloor;
@@ -25,15 +31,55 @@ public class AbilitySystem : MonoBehaviour
 
     void SpawnObject(GameObject hazard)
     {
-        int x = Mathf.RoundToInt(transform.position.x);
-        int z = Mathf.RoundToInt(transform.position.z);
-        Vector3 SpawnPos = new Vector3(x, transform.position.y, z);
         Vector2 dir = movement.GetDir().normalized;
         dir.x = Mathf.CeilToInt(dir.x);
         dir.y = Mathf.CeilToInt(dir.y);
         if (dir == Vector2.zero)
-            dir = Vector2.up;
+            dir = Vector2.down;
+        string state = "";
+        if (dir.y >= 1)
+        {
+            state = "Up";
+            mop.transform.localPosition = mopPositions[0];
+        }
+        else if (dir.y <= -1) 
+        {
+            state = "Down";
+            mop.transform.localPosition = mopPositions[1];
+        }
+        else if (dir.x >= 1)
+        {
+            state = "Right";
+            mop.transform.localPosition = mopPositions[2];
+        }
+        else
+        {
+            state = "Left";
+            mop.transform.localPosition = mopPositions[3];
+        }
+        mop.SetActive(true);
+        animator.SetTrigger(state);
+        float length = 0;
+        RuntimeAnimatorController ac = animator.runtimeAnimatorController;
+        for (int i = 0; i < ac.animationClips.Length; i++)
+        {
+            if (ac.animationClips[i].name == state + "Anim")
+            {
+                length = ac.animationClips[i].length;
+            }
+        }
+        IEnumerator coroutine = MopTime(hazard, new Vector3(dir.x, 0, dir.y), length);
+        StartCoroutine(coroutine);
+    }
+
+    IEnumerator MopTime(GameObject hazard, Vector3 dir, float length)
+    {
+        yield return new WaitForSeconds(length);
+        mop.SetActive(false);
+        int x = Mathf.RoundToInt(transform.position.x);
+        int z = Mathf.RoundToInt(transform.position.z);
+        Vector3 SpawnPos = new Vector3(x, transform.position.y, z);
         GameObject newObj = Instantiate(hazard);
-        newObj.transform.position = SpawnPos + new Vector3(dir.x, 0, dir.y);
+        newObj.transform.position = SpawnPos + dir;
     }
 }
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -12,15 +12,26 @@ public class PlayerMovement : MonoBehaviour
 
     [SerializeField]
     private float speed;
+    [SerializeField]
+    GameObject playerSprite;
+    Animator animator;
+    SpriteRenderer spriteRenderer;
+    //0 - up
+    //1 - down
+    //2 - right
+    //3 - left
+    [SerializeField]
+    Sprite[] BaseSprites;
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        animator = playerSprite.GetComponent<Animator>();
+        spriteRenderer = playerSprite.GetComponent<SpriteRenderer>();
         controls = new InputController();
         controls.PlayerMovement.Movement.performed += Move;
         controls.PlayerMovement.Movement.canceled += StopMove;
         controls.PlayerMovement.Movement.Enable();
-
         //controls.UI.Pause.performed += TogglePause;
         //controls.UI.Pause.Enable();
     }
@@ -29,10 +40,48 @@ public class PlayerMovement : MonoBehaviour
     {
         dir = speed * ctx.ReadValue<Vector2>().normalized;
         rb.velocity = new Vector3(dir.x, 0, dir.y);
+        string state = "";
+        if (dir.x < 0)
+        {
+            state = "WalkRight";
+        }
+        else if (dir.x > 0)
+        {
+            state = "WalkLeft";
+        }
+        else if (dir.y > 0)
+        {
+            state = "WalkUp";
+        }
+        else
+        {
+            state = "WalkDown";
+        }
+        if (animator.enabled == false)
+            animator.enabled = true;
+        animator.Play(state);
     }
 
     void StopMove(CallbackContext ctx)
     {
+        Vector3 vel = rb.velocity;
+        animator.enabled = false;
+        if (vel.x < 0)
+        {
+            spriteRenderer.sprite = BaseSprites[2];
+        }
+        else if (vel.x > 0)
+        {
+            spriteRenderer.sprite = BaseSprites[3];
+        }
+        else if (vel.z < 0)
+        {
+            spriteRenderer.sprite = BaseSprites[0];
+        }
+        else
+        {
+            spriteRenderer.sprite = BaseSprites[1];
+        }
         rb.velocity = Vector3.zero;
     }
 
